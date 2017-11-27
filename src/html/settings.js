2fd12cdec6fb777c3ccbca9de81da9d291799ee9
BorisKolyarov <9309706@gmail.com>
2017-11-27 21:49:20 +0200

    runtime fix

--- src/html/settings.js
+++ src/html/settings.js
@@ -11,17 +11,16 @@ function saveOptions(e) {
     windowOpacity: $("#windowOpacity").val(),
     timerTick: $("#timerTick").val(),
     showRuntime: $("#showRuntime").prop('checked'),
+    speedFormat: $('input[name="speedFormat"]:checked').val(),
   };
 
   chrome.storage.local.set(elements);
 }
 
 function restore() {
-  var items = ["headerColor", "headerOpacity", "windowColor", "windowOpacity", "timerTick", "showRuntime"];
+  var items = ["headerColor", "headerOpacity", "windowColor", "windowOpacity", "timerTick", "showRuntime", "speedFormat"];
 
   var onGet = items => {
-
-    console.log(items);
 
     if (items.headerColor)
       $("#headerColor").val(items.headerColor);
@@ -35,6 +34,10 @@ function restore() {
       $("#timerTick").val(items.timerTick);
     if (items.showRuntime)
       $("#showRuntime").prop('checked', true);
+    if (items.speedFormat) {
+      let sel =`#speedFormat_${items.speedFormat}`;
+      $(sel).prop('checked', true);
+    }
   };
 
   chrome.storage.local.get(items, onGet);
